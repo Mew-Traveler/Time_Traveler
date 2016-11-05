c13b0587d698270988b5e7c23f5f2084d33efb0b
Yvonne <tailcloud14916@gmail.com>
2016-11-05 14:07:04 +0800

    basic traffic result parse

--- lib/Time_Traveler/trafficinfo.rb
+++ lib/Time_Traveler/trafficinfo.rb
@@ -4,23 +4,36 @@ module Google
   class TrafficInfo
     attr_reader :infos
     attr_reader :origins, :dest, :mode
+    attr_reader :anaDistance, :anaDuration, :fare
 
 
     def initialize(distance,search)
+
       parseSearch(search)
       @googleapi = ENV['GOOGLE_API']
       @infos = distance.map{ |item|
         infos = info(item)
       }
+      @info = distance[0]
     end
 
     def trafficAnaly
-      @infos
+      @anaDistance = @info['distance']['value']
+      @anaDuration = @info['duration']
+      if(@info['fare'])
+        @fare = @info['fare']
+      end
+
+      @info
+
     end
 
     def self.find(origins:,destinations:,mode:)
       distance_data = GoogleApi.distanceInfo(origins,destinations,mode)
       @search_info = {googleapi:ENV['GOOGLE_API'],originsVal:origins,destVal:destinations,modeVal:mode}
+    #  @info = distance_data[0]
+    #  print @info.to_s
+
       new(distance_data,@search_info)
     end
 
@@ -32,7 +45,8 @@ module Google
     end
 
     def info(item)
-      info = item[0]
+      info = item
+
     end
 
   end
